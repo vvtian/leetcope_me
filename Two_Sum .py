275e53e06abb5f27ab740ef3a852dfb06218b1ea
vvtian <398310343@qq.com>
2018-02-22 20:12:38 +0800

    Given an array of integers, return indices of the two numbers such that they add up to a specific target
    for循环中元素ele和它的索引index，从list中除去ele形成check_list，从中搜索满足条件的ans。

---
+++ Two_Sum .py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for index,ele in enumerate(nums):
+            ans = target - ele
+            check_list = nums[:index] + nums[index+1:]
+            print(check_list)
+            if ans in check_list and (ele+ans)== target:
+                return [index,nums.index(ans,index+1)]
+            else:
+                ans = 0
+            return 0
+
+
+
+y = Solution().twoSum([2, 7, 11, 15],9)
+print(y)
+
+
